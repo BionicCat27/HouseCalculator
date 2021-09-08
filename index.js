9f48415138e34a1a4e91eb14b4cf696696967627
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-09-08 23:50:09 +1200

    Mortgage calculator logic

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ initListeners();
 
 function initListeners() {
     addSubmitListener();
+    priceChangeListener();
 }
 
 function addSubmitListener() {
     $('#submit-btn').click(function() {
+        priceChangeListener();
         calculateSimpleTime();
+        calculateMortgagedTime();
+    });
+}
+
+function priceChangeListener() {
+    $('#targetprice').change(function() {
+        var price = $(this).val();
+        var minDeposit = $('#deposit-result');
+        minDeposit.text("Minimum deposit (20%): $" + ((price / 100) * 20));
     });
 }
 
@@ -16,6 +27,25 @@ function calculateSimpleTime() {
     var targetprice = document.getElementById("targetprice").value;
 
     var result = (targetprice-savings) / salary;
-    var resultEl = document.getElementById("result");
-    resultEl.innerText = "Result: " + result + " years";
+    var resultEl = document.getElementById("simple-result");
+    resultEl.innerText = "Simple purchase: " + result + " years of saving";
+}
+
+function calculateMortgagedTime() {
+    var savings = document.getElementById("savings").value;
+    var salary = document.getElementById("salary").value;
+    var targetprice = document.getElementById("targetprice").value;
+    var interestrate = document.getElementById("interest-rate").value;
+
+    var postdepositprice = targetprice - (savings);
+    var years = 0;
+    while(postdepositprice > 0) {
+        years++;
+        postdepositprice -= salary;
+        postdepositprice = Math.pow(postdepositprice, 1 + (interestrate / 100));
+    }
+
+    var result = years;
+    var resultEl = document.getElementById("mortgaged-result");
+    resultEl.innerText = "Mortgaged purchase: " + result + " years until paid off";
 }
